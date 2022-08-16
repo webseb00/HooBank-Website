ded97de557ab5b85fef3766413b670bbcb7bf1ed
webseb00 <lordow91@gmail.com>
2022-08-16 13:48:04 +0200

    Create testimonials

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
-  )
-}
+const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] sm:mr-2 mr-0 my-5 feedback-card cursor-pointer">
+    <img src={quotes} alt="''" className="w-[42px] h-[27px] object-contain" />
+    <p className="font-poppins font-normal leading-[32px] text-white text-[18px] my-10">{content}</p>
+
+    <div className="flex flex-row">
+      <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold leading-[32px] text-white text-[20px]">{name}</h4>
+        <p className="font-poppins font-normal leading-[24px] text-dimWhite text-[16px]">{title}</p>
+      </div>
+    </div>
+  </div>
+)
 
 export default FeedbackCard
